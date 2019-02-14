3f66c87a66cd23855c01234c2eefc7f132bf873d
lsttsl2019 <dlamtd123@naver.com>
2019-02-14 16:09:49 +0900

    첫번째입니다

--- app/src/main/java/com/isttis2019/projectaccountbook/IntroActivity.java
+++ app/src/main/java/com/isttis2019/projectaccountbook/IntroActivity.java
@@ -1,13 +1,47 @@
 package com.isttis2019.projectaccountbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class IntroActivity extends AppCompatActivity {
+
+
+  ImageView iv;
+
+
+Timer timer=new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        iv=findViewById(R.id.iv);
+
+        Animation ani= AnimationUtils.loadAnimation(this,R.anim.appear_logo);
+        iv.startAnimation(ani);
+
+        timer.schedule(task, 4000);
+
+
     }
+
+    TimerTask task=new TimerTask() {
+        @Override
+        public void run() {
+            Intent intent=new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+
+        }
+    };
+
 }
